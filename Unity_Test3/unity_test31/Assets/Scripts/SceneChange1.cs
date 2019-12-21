d5cdbdd1b13c731f3047c1d2d1e8d1b59b2ebccd
rebuilder945 <1278950828@qq.com>
2019-12-21 18:24:39 +0800

    res1

---
+++ Unity_Test3/unity_test31/Assets/Scripts/SceneChange1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneChange1 : MonoBehaviour
+{
+
+    public void OnClick1()
+    {
+        SceneManager.LoadScene("456");
+    }
+}
